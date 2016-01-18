03c0712a9569c21c6e0b8d5f43a2cb9549575cd3
neonprimetime security (Justin C Miller) <neonprimetime.geo@yahoo.com>
2016-01-18 14:40:50 -0600

    Updated Future page from Qualys API v1 to v2
    Updated Future page to use newer Qualys API v2 instead of v1

--- www/future/index.php
+++ www/future/index.php
@@ -1,7 +1,7 @@
-<?php
+<?
     # ===========================================================
-    # file name:  future/index.php
-    # purpose:    main page to display scans that are scheduled
+    # file name:  present/index.php
+    # purpose:    main page to display currently running scans
     # created:    June 2011
     # authors:    Don Franke
     #             Josh Stevens
@@ -17,208 +17,185 @@
   Licensed under the MIT license: http://www.opensource.org/licenses/mit-license.php 
   ---------------------------------------------------------------->
 
-<?include '../creds.php'?>
+<? include '../creds.php'?>
+
 <?
-    $pageName = "future";
-    
-    //$url = "https://qualysapi.qualys.com/msp/scheduled_scans.php?type=scan&active=yes";
-    $url = $apiprefix."/msp/scheduled_scans.php?type=scan&active=yes";
+    $url = "https://qualysapi.qg2.apps.qualys.com/api/2.0/fo/schedule/scan/?action=list&active=1"; // Qualys API v2 only running scans
     $error = "";
     $aryData = array();
     
     // set URL and other appropriate options
     $ch = curl_init(); 
-    
     curl_setopt($ch, CURLOPT_URL, $url);
-    curl_setopt($ch, CURLOPT_HEADER, false);
     curl_setopt($ch, CURLOPT_USERPWD, $username .':'.$password); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
-	// extra setings added
-	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
-	curl_setopt($ch, CURLOPT_PROXY, $proxy);
-	curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyuserpwd);
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-    // grab XML data
-    $xmldata = curl_exec($ch);
+	// added these    
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_PROXY, $proxy);
+        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyuserpwd);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
 
-	if(!$xmldata) {
-		print "!!!!!!!!!!!!!!!ERROR DOWNLOADING XML!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+	// added these for api 2
+	curl_setopt($ch, CURLOPT_HEADER, 1);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, array('X-Requested-With: Curl'));
+
+    	// grab XML data
+	$xmldata = curl_exec($ch);
+
+	//print $xmldata; // DEBUG
+
+	$indexof = strrpos($xmldata, "<?xml version");
+	$xmldata = substr($xmldata, $indexof);		// strip off the extra text before the xml
+
+	//print $xmldata; // DEBUG
+
+
+	if(!$xmldata){
 		print curl_error($ch);
 	}
-	else{
-		//print $xmldata; // debug only
-	}
-
 
     $tag_tree = array();
     $stack = array();
     $i=0;
-    
+
     # this class is for each tag found in the XML return
-    class tag {
+   class tag {
         var $name;
-        var $attrs;
+        var $attrs=array();
         var $children;
     
         # this is when a tag is found
         function tag($name, $attrs, $children) {
             global $currenttag;
+            global $currentvalue;
             global $error;
-            global $i;
             global $aryData;
-            if(trim($currenttag)!=""&&trim($name)!="") {
-                # do nothing
-            }
-    
-            # handle error such as cannot reach API
-            if($currenttag=="ERROR"&&trim($name)!="") {
-               $error = trim($name);
-            }
+            global $i;
 
-            # capture targets
-            if($currenttag=="TARGETS"&&trim($name)!="SCHEDULE"&&trim($name)!="") {
-               $name = str_replace(",",", ",$name);
-               if(strlen($aryData[$i][1])!=0) {
-                   $aryData[$i][1] .= trim($name);
-               } else {
-                   $aryData[$i][1] = trim($name);
-               }
-            }
+            $currenttag = trim($currenttag);
+            $currentvalue = trim($currentvalue);
+            $name = trim($name);
+   
+	    # capture v2 next launch date
+	    if($currenttag=="NEXTLAUNCH_UTC"&&$name!=""&&$name!="TIME_ZONE"){
+		$timestamp = strtotime($name);
+		$aryData[$i][0] = date("m-d h:i:s a", $timestamp)." CST";
+	    }
 
-            # capture next run date
-            if($currenttag=="NEXTLAUNCH_UTC") {
-               $name = str_replace("OPTION","",$name);
-               if(trim($name)!=""&&trim($name)!="DEFAULT_SCANNER") {
-                $timestamp = strtotime($name." GMT");
-		$aryData[$i][0] = $timestamp;
-//                $aryData[$i][0] = date("m-d h:i:s a", $timestamp);
+	    # catpure v2 profile
+	    if($currenttag=="TITLE"&&$name!=""&&$name!="USER_LOGIN"&&$name!="DEFAULT_FLAG"){
+		if($aryData[$i][4]==""){
+			$aryData[$i][4] = $name;		
+		}
+	    }
 
-//                    $aryData[$i][0] = trim($name);
-				
-//					$finishTime= $aryData[$i][0];   
-//					$temp = split("-",$finishTime);
-//					$year = $temp[0];
-//					$month = $temp[1];
-//					$day = substr($temp[2],0,2);
-			
-//					$temp = split(":",$finishTime);
-//					$hour = $temp[0];
-//					$hour = substr($temp[0],12,strlen($hour)-12);
-//					if(strlen($hour)==1) {
-//						$hour = "0" . $hour;
-//					}
-//					$minute = $temp[1];
-//					$second = substr($temp[2],0,2);
-			
-//					$finishTime = date("Y\-m\-d H:m:s", mktime($hour-5,$minute,$second,$month,$day,$year));
-				
-//					$aryData[$i][0]= $finishTime;
-               }
-            }
+	    # capture v2 status
+	    if($currenttag=="ACTIVE"&&$name!=""&&$name!="TITLE"){
+		if($name==1) { $aryData[$i][3] = "ACTIVE"; }
+	    }
+ 
+	    # capture v2 targets 
+	    if($currenttag=="TARGET"&&$name!=""&&$name!="ISCANNER_NAME"){
+		$aryData[$i][2] = $name;
+	    }
 
-            # capture title
-            if($currenttag=="TITLE") {
-               if(trim($name)!=""&&trim($name)!="TARGETS") {
-		    $aryData[$i][2] = "SCHEDULED";
-                    $aryData[$i][3] = trim($name);
-               }
-            }
-
-            # capture options
-            if($currenttag=="OPTION_PROFILE_TITLE") {
-               if(trim($name)!=""&&trim($name)!="SCAN") {
-                   //$aryData[$i][3] = trim($name);
-                   $i++;
-               }
-            }
+	    # capture end of record
+	    if($currenttag=="DST_SELECTED"&&$name!=""&&$name!="SCAN"){
+		$i++;
+	    }
         }
     }
-    
+
     # function used to parse XML
     function startTag($parser, $name, $attrs) {
         global $tag_tree, $stack;
         $tag = new tag($name,$attrs,'');
         global $currenttag;
+        global $currentvalue;
         $currenttag = $name;
+        array_push($stack,$tag);
+        $element = array();
+        $element['name'] = $name;
+        foreach ($attrs as $key => $value) { 
+            $element[$key]=$value;
+            $currentvalue=$value;
+        }
     }
-    
+
     # function used to parse XML
     function endTag($parser, $name) {
-        # do nothing
-    }
-    
+        global $stack;
+        $stack[count($stack)-2]->children[] = $stack[count($stack)-1];
+    }   
+
     # function used to parse XML
-    function cdata($parser, $element) 
-    {
+    function cdata($parser, $element) {
         global $tag_tree, $stack;
         $tag = new tag($element,$attrs,'');
     }
-    
-    #create xml parser
+
+    # create XML parser and define handlers
     $xml_parser = xml_parser_create();
     xml_set_element_handler($xml_parser, "startTag", "endTag");
     xml_set_character_data_handler($xml_parser, "cdata");
-    
     $data = xml_parse($xml_parser,$xmldata);
-    global $iAryTotal;
+
+    global $dataTotal;
     global $i;
-    $iAryTotal = $i+1;
+    $dataTotal = $i;
     
     if(!$data) {
         die(sprintf("XML error: %s at line %d",xml_error_string(xml_get_error_code($xml_parser)),xml_get_current_line_number($xml_parser)));
     }
     
-    # release the kraken! (parser)
+    # release the parser!
     xml_parser_free($xml_parser);
-    
-    $dataTotal = $i;
 ?>
-  <html>
-	<head>
-		<link rel=stylesheet href=../scantronitor.css>
-		</link>
-		<title>Scantronitor</title>
-	</head>
-	<body>
-		<?include 'header.php'?>
-		
-<h2 align=center><?=$error?></h2>
-<p align=center>
-	<table id=datatable width=1200>
-		<tr>
-			<th width=150><img src="../images/datetime.png" alt="DateTime"></th> 
-			<th width=100><img src="../images/type.png" alt="Type"></th>
-			<th width=350><img src="../images/profile.png" alt="Profile"></th>
-			<th width=600><img src="../images/targets.png" alt="Targets"></th>
-		</tr>
-		
-		<?global $aryData;
-		  global $i;
-		$visibleCount = 0;
-		usort($aryData, function($a, $b) {
-			return $a[0] - $b[0];
-		});
-		  for($i=0;$i<$dataTotal;$i++) {
-			if(strlen($aryData[$i][0]) > 0){
-				$visibleCount = $visibleCount + 1;
-		?>
-			<tr onMouseOver="this.bgColor='#EDEDED'" onMouseOut="this.bgColor='#ffffff'">
-				<td><?=date("m-d h:i:s a", $aryData[$i][0]) ?> CST</td>
-                <td><?=$aryData[$i][2] ?></td>
-                <td><?=$aryData[$i][3] ?></td>
-				<td>
-				<?=$aryData[$i][1] ?>
-				</td>
-			</tr>
-			<? } ?>
-		<?}?>
-		<tr>
-			<th colspan=4 align=center><?=$visibleCount?> Total Record(s)</th>
-		</tr>
-	</table>
-</p>
+  
+<html>
+<head>
+<link rel="stylesheet" href="../scantronitor.css"> 
+</link> 
+<title>Scantronitor</title> </head> 
+<body>
+<?include 'header.php'?>
+	
+<?if($error!="") {?>	
+    <h2 align="center">No scan or map currently running</h2>
+<?} else {?>
+    <p align="center">
+    <table id="datatable" width="1200">
+	<tr>
+	    <th width="150"><img src="../images/datetime.png" alt="DateTime"></th>
+	    <th width="150"><img src="../images/profile.png" alt="Profile"></th>
+	    <th width="650"><img src="../images/targets.png" alt="Targets"></th>
+	    <th width="250"><img src="../images/status.png" alt="Status"></th>
+	</tr>
 
+<?global $aryData;
+  global $i;
+  $extraMessage = "";
+  if($dataTotal == 0){
+	$extraMessage = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;NO SCANS CURRENTLY RUNNING. Please check the 'Past' or 'Future' page for previously run or not yet run scans.";
+  }
+  for($i=0;$i<$dataTotal;$i++) {
+      $aryData[$i][2] = str_replace(",",", ",$aryData[$i][2])?>
+	<tr onMouseOver="this.bgColor='#dee7d1'" onMouseOut="this.bgColor='#ffffff'">
+	    <td align="left"><?=$aryData[$i][0] ?></td>
+	    <td align="left"><?=$aryData[$i][4] ?></td>
+	    <td align="left"  width="600"><?=$aryData[$i][2] ?></td>
+	    <td align="left"><?=$aryData[$i][3] ?></td>	
+	</tr> 
+	<?}?>
+	<tr>
+	    <th colspan="4" align="center"><?=$dataTotal?> Total Record(s)<?=$extraMessage?></th>
+	</tr>
+    </table>
+    </p>
+<?}?>
+		
 <?include '../footer.php'?>
+	
 </body>
 </html>
